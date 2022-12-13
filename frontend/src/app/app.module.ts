3c19bddbc3343150038f744ab50cfb4013707aeb
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2022-12-13 15:24:04 -0300

    criacao componente nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,25 @@ import { HeaderComponent } from './components/templat/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/templat/footer/footer.component';
+import { NavComponent } from './components/templat/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
