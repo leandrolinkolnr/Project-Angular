79c37edf45f133fa7b348f631aa5044cc9fd439e
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2022-12-20 15:41:33 -0300

    Componente Obter Produto

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -26,4 +26,11 @@ export class ProductService {
     return this.http.post<Product>(this.baseUrl, product)
   }
       // retorna um observable product
+
+
+read(): Observable<Product[]> {
+  return this.http.get<Product[]>(this.baseUrl);
+}
+
+
 }
