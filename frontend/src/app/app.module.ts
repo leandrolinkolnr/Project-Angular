e31849e1f5b12a96424017011ddcd359cc5ebc3f
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2022-12-21 15:15:41 -0300

    revert

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,10 +27,6 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -43,8 +39,7 @@ import { MatSortModule } from '@angular/material/sort';
     RedDirective,
     ForDirective,
     ProductCreateComponent,
-    ProductReadComponent,
-    ProductRead2Component
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
@@ -59,10 +54,7 @@ import { MatSortModule } from '@angular/material/sort';
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
