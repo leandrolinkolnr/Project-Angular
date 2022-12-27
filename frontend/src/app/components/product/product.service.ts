820b425d24f089f5d2410789565d8f3613e39682
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2022-12-27 16:33:51 -0300

    Atualização de products ok

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Product } from './product.model';
 
 @Injectable({
@@ -30,6 +30,23 @@ export class ProductService {
 
 read(): Observable<Product[]> {
   return this.http.get<Product[]>(this.baseUrl);
+}
+
+readById(id: number): Observable<Product> {
+  const url = `${this.baseUrl}/${id}`;
+  return this.http.get<Product>(url)
+}
+
+
+update(product: Product): Observable<Product> {
+  const url = `${this.baseUrl}/${product.id}`;
+  return this.http.put<Product>(url, product)
+}
+
+
+delete(id: number): Observable<Product> {
+  const url = `${this.baseUrl}/${id}`;
+  return this.http.delete<Product>(url)
 }
 
 
